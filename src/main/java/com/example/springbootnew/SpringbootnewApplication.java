681e1f46bf520ec45013020af0891b60682b8745
Lucas756 <salleslucas00@gmail.com>
2023-11-25 12:10:01 -0300

    crud

---
+++ src/main/java/com/example/springbootnew/SpringbootnewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootnewApplication.class, args);
+	}
+
+}
